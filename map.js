f64dfbb4232101a97f61e99bc401c3ccf7c8a59d
MOGLAS999 <gerogero7772002@yahoo.co.jp>
2017-05-21 18:48:17 +0900

    検索フォームを追加
    初期位置に戻るボタンも追加

--- map.js
+++ map.js
@@ -19,7 +19,52 @@ function initMap(){
   map.addListener('click', function(e) {
     placeMarkerAndPanTo(e.latLng, map);
   });
+
+  // フォームのsubmitイベントに検索処理をセット
+  document.getElementById("form").addEventListener('submit', function(e) {
+    searchByAddress();
+  });
+
+  document.getElementById("initPos").addEventListener('click', function(e) {
+    initMap();
+  });
 }
+
+function searchByAddress() {
+  // フォームに入力された住所情報を取得
+  var address = document.getElementById("address").value;
+
+  var geocoder = new google.maps.Geocoder();
+
+  var opts = {
+    zoom: 18,
+    center: {lat: 35.642048, lng: 139.713459}
+  };
+  var map = new google.maps.Map(document.getElementById("map"), opts);
+
+  var latlngFrom = new google.maps.LatLng(35.648579, 139.707181);
+  var latlngTo   = new google.maps.LatLng(35.636827, 139.723617);
+  //プレイスを優先的に検索する領域
+  var bounds = new google.maps.LatLngBounds(latlngFrom, latlngTo);
+
+  geocoder.geocode({
+    address: address,
+    bounds: bounds,
+    componentRestrictions: {
+      country: 'JP'
+    } 
+  }, function(results, status) {
+    if(status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+
+      placeMarkerAndPanTo(results[0].geometry.location, map);
+      console.log('Geocode was successful: ' + status);
+    } else {
+      console.log('Geocode was not successful: ' + status);
+    }
+  });
+}
+
 
 function placeMarkerAndPanTo(latLng, map) {
   // 全てのマーカーを削除
